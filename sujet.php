f2dc0c98b8e31e2f014bc6b990a9b638cf7cb80a
dulphMain <augustin.michau@outlook.com>
2025-12-29 15:19:47 +0100

    fix du LaTeX ne fonctionnant pas sur PC
    Remplacement du http par https pour éviter une erreur de chargement du script :
    
    Mixed Content: The page at '<a target="_blank" rel="noopener" class="" href="https://theverylittlewar.com/sujet.php?id=988'">https://theverylittlewar.com/sujet.php?id=988'</a> was loaded over HTTPS, but requested an insecure script '<a target="_blank" rel="noopener" class="" href="http://cdn.mathjax.org/mathjax/latest/MathJax.js?config=TeX-AMS-MML_HTMLorMML'">http://cdn.mathjax.org/mathjax/latest/MathJax.js?config=TeX-AMS-MML_HTMLorMML'</a>. This request has been blocked; the content must be served over HTTPS.

--- sujet.php
+++ sujet.php
@@ -243,4 +243,4 @@ if (isset($_GET['id'])) {
 
 
 include("includes/copyright.php"); ?>
-<script type="text/javascript" src="http://cdn.mathjax.org/mathjax/latest/MathJax.js?config=TeX-AMS-MML_HTMLorMML"></script>
+<script type="text/javascript" src="https://cdn.mathjax.org/mathjax/latest/MathJax.js?config=TeX-AMS-MML_HTMLorMML"></script>
